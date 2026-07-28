5623fe2c48b1068e4ec8681c9dc7683bd6590c34
SilverFennec <123173551+SilverFennec@users.noreply.github.com>
2026-07-28 17:04:53 +0000

    Add logic to evaluate round winner

--- script.js
+++ script.js
@@ -25,3 +25,39 @@ function getUserChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+  
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You lose! Paper beats rock"); 
+    computerScore++;
+  }
+  else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! Rock beats scissors"); 
+    humanScore++; 
+  }
+  
+  else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You lose! Scissors beat paper");
+    computerScore++; 
+  }
+  else if (humanChoice == "paper" &&  computerChoice == "rock") {
+    console.log("You win! Paper beats rock");
+    humanScore++; 
+  }
+  
+  else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You lose! Rock beats scissors")
+    computerScore++; 
+  }
+  else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! Scissors beat paper");
+    humanScore++; 
+  }
+  
+  else if (humanChoice == computerChoice) {
+    console.log("Tie");
+    humanScore++;
+    computerScore++; 
+  }
+  
+}
